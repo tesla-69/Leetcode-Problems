1ea0888a8e4d9a31dca4828102adce0c252cce53
Kartik shah <123581808+KartikShah369@users.noreply.github.com>
2024-07-23 00:02:29 +0530

    Time: 28 ms (64.96%) | Memory: 26.4 MB (65.45%) - LeetSync

---
+++ 2502-sort-the-people/sort-the-people.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        map<int,string> mp;
+        for(int i=0;i<names.size();i++){
+            mp[heights[i]] = names[i];
+        }
+        sort(heights.begin() , heights.end());
+        reverse(heights.begin() , heights.end());
+        for(int i=0;i<names.size();i++){
+            names[i] = mp[heights[i]];
+        }
+        return names;
+    }
+};
