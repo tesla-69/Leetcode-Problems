eaaf2be58b9c2ae7c6dd9948a432cb407c22c6f6
Kartik shah <123581808+KartikShah369@users.noreply.github.com>
2024-08-08 22:06:36 +0530

    Time: 145 ms (39.07%) | Memory: 62.7 MB (49.50%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -24,20 +24,48 @@ public:
 
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n = graph.size();
-        vector<int> vis(n, 0);
-        vector<int> pathvis(n, 0);
-        vector<int> safe(n, 0);
-        vector<int> ans;
-        for(int i =0 ;i <n;i++){
-            if(!vis[i]){
-                dfs(i,vis,pathvis,graph,safe);
+        vector<int> adj[n];
+        vector<int> indegree(n,0) ;
+        for(int i=0; i<n ; i++) {
+            for(auto j : graph[i]){
+                adj[j].push_back(i);
+                indegree[i]++;
             }
         }
-        for(int i=0 ; i<n;i++ ) {
-            if(safe[i]==1){
-                ans.push_back(i);
+        queue<int> q;
+        for(int i =0 ;i<n;i++){
+            if(indegree[i] == 0) q.push(i);
+        }
+        vector<int>ans;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+            for(auto it : adj[node]) {
+                indegree[it]--;
+                if(indegree[it] == 0) q.push(it);
             }
         }
+        sort(ans.begin() ,ans.end());
         return ans;
+        
+        
+        
+        // int n = graph.size();
+        // vector<int> vis(n, 0);
+        // vector<int> pathvis(n, 0);
+        // vector<int> safe(n, 0);
+        // vector<int> ans;
+        // for(int i =0 ;i <n;i++){
+        //     if(!vis[i]){
+        //         dfs(i,vis,pathvis,graph,safe);
+        //     }
+        // }
+        // for(int i=0 ; i<n;i++ ) {
+        //     if(safe[i]==1){
+        //         ans.push_back(i);
+        //     }
+        // }
+        // return ans;
     }
 };
