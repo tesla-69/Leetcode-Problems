726589434c3a212a23e013a68d8d4d471e5c2fb0
Kartik shah <123581808+KartikShah369@users.noreply.github.com>
2024-08-18 13:37:17 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (99.21%) - LeetSync

--- 264-ugly-number-ii/ugly-number-ii.cpp
+++ 264-ugly-number-ii/ugly-number-ii.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
     int nthUglyNumber(int n) {
-        vector<int> primes = {2, 3, 5};
-        priority_queue<long, vector<long>, greater<long>> uglyHeap;
-        unordered_set<long> visited;
-        
-        uglyHeap.push(1);
-        visited.insert(1);
-        
-        long curr;
-        for (int i = 0; i < n; ++i) {
-            curr = uglyHeap.top();
-            uglyHeap.pop();
-            for (int prime : primes) {
-                long new_ugly = curr * prime;
-                if (visited.find(new_ugly) == visited.end()) {
-                    uglyHeap.push(new_ugly);
-                    visited.insert(new_ugly);
-                }
-            }
+        int arr[n+1];
+        arr[1]=1;
+        int i2=1, i3=1, i5=1; 
+
+        for(int i=2; i<=n; i++){
+            int val[3] = {arr[i2]*2, arr[i3]*3, arr[i5]*5};
+
+            arr[i] = min({val[0], val[1], val[2]});
+            
+            if(arr[i]==val[0]) i2++;
+            if(arr[i]==val[1]) i3++;
+            if(arr[i]==val[2]) i5++;
         }
-        return (int)curr;
+
+        return arr[n];
     }
 };
