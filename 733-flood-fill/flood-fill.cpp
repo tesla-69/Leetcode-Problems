c8012d561e06f39c7a4e1b1076d825d26eaf22f6
Kartik shah <123581808+KartikShah369@users.noreply.github.com>
2025-01-13 22:11:59 +0530

    Time: 0 ms (100.00%) | Memory: 18.4 MB (19.76%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,31 +1,28 @@
 class Solution {
-public:
- void dfs(int i, int j, vector<vector<int>>& image, vector<vector<int>>& vis
-    , vector<vector<int>>& copy, int color, int newColor, int n, int m){
-        vis[i][j] = 1;
-        copy[i][j] = newColor;
-        for(int a=-1;a<=1;a++){
-            for(int b=-1;b<=1;b++){
-                if(abs(a) == 1 && abs(b) == 1) continue;
-                int nsr = i + a;
-                int nsc = j + b;
-                if(nsr>=0 && nsr < n && nsc>=0 && nsc<m &&
-                image[nsr][nsc] == color && !vis[nsr][nsc]){
-                    dfs(nsr,nsc,image,vis,copy,color,newColor,n,m);
+private:
+    void dfs(vector<vector<int>>& image,vector<vector<int>>& copy, vector<vector<int>>& vis, int r, int c, int n, int m, int color, int presentcolor) {
+        vis[r][c] = 1;
+        copy[r][c] = color;
+        for(int i = -1 ; i <= 1; i++){
+            for(int j =-1 ; j <= 1; j++){
+                if(abs(i) == abs(j)) continue;
+                int nr = r + i;
+                int nc = c + j;
+                if(nr >= 0 && nr < n && nc >= 0 && nc < m && !vis[nr][nc] && image[nr][nc] == presentcolor){
+                    dfs(image , copy,vis,nr,nc,n,m,color,presentcolor);
                 }
             }
         }
     }
+
+public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int n = image.size();
         int m = image[0].size();
-        vector<vector<int> > copy = image;
-        vector<vector<int> > vis(n,vector<int>(m,0));
-        
-        int oldcolor = image[sr][sc];
-        
-        dfs(sr,sc,image,vis,copy,oldcolor,color,n,m);
-        
+        vector<vector<int>> copy = image;
+        int presentcolor = image[sr][sc];
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        dfs(image,copy, vis, sr, sc, n, m, color, presentcolor);
         return copy;
     }
 };
