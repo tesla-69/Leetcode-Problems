4e696fae01a90d2ab504734400ee0f9914fd248d
Kartik shah <123581808+KartikShah369@users.noreply.github.com>
2025-01-13 21:07:33 +0530

    Time: 24 ms (79.15%) | Memory: 17.5 MB (48.14%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,42 +1,29 @@
 class Solution {
-public:
-
-    void bfs(vector<vector<char> > &grid , vector<vector<int> > &vis
-    , int row , int col , int n , int m){
-        vis[row][col] = 1;
-        queue<pair<int , int>> q;
-        q.push({row,col});
-        
-        while(!q.empty()){
-            int i = q.front().first;
-            int j = q.front().second;
-            q.pop();
-            for(int a = -1;a<=1;a++){
-                for(int b= -1;b<=1;b++){
-                    if(abs(a) == 1 && abs(b) == 1) continue;
-                    int nrow = i + a;
-                    int ncol = j + b;
-                    if(nrow>=0 && nrow<n && ncol >= 0 && ncol < m &&
-                    grid[nrow][ncol] == '1' && !vis[nrow][ncol]){
-                        q.push({nrow,ncol});
-                        vis[nrow][ncol] = 1;
-                    }
+private: 
+    void dfs(vector<vector<char>> &grid , vector<vector<int>> &vis, int r , int c, int n , int m){
+        vis[r][c] = 1;
+        for(int i = -1 ; i <= 1 ; i++){
+            for(int j = -1;j<=1;j++) {
+                if(abs(i) == abs(j)) continue;
+                int nr = r + i;
+                int nc = c + j;
+                if(nr >= 0 && nr<n && nc >= 0 && nc < m && !vis[nr][nc] && grid[nr][nc] == '1'){
+                    dfs(grid,vis,nr,nc,n,m);
                 }
             }
         }
-        
     }
+public:
     int numIslands(vector<vector<char>>& grid) {
-         int n = grid.size();
+        int n = grid.size();
         int m = grid[0].size();
-        vector<vector<int> > vis(n , vector<int>(m , 0));
-        
+        vector<vector<int>> vis(n,vector<int> (m,0));
         int cnt = 0;
-        
-        for(int i=0;i<n;i++) {
-            for(int j=0;j<m;j++){
-                if(grid[i][j] == '1' && !vis[i][j]) {
-                    bfs(grid , vis, i , j, n , m);
+        for(int i = 0;i<n;i++){
+            for(int j =0;j<m;j++){
+                if(!vis[i][j] && grid[i][j] == '1') {
+                    dfs(grid,vis,i,j,n,m);
+                    cout<<i<<" "<<j<<endl;
                     cnt++;
                 }
             }
