38cf80119f5e2726559dba28a22ca7035634cf0c
Kartik shah <123581808+KartikShah369@users.noreply.github.com>
2025-01-17 00:47:44 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (31.86%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -1,47 +1,44 @@
 class Solution {
 public:
     void solve(vector<vector<char>>& board) {
-         queue<pair<int,int>> q;
-         int n = board.size();
-         int m = board[0].size();
-        vector<vector<int> > vis(n,vector<int>(m,0));
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(i==0 || i == n-1 || j==0 || j==m-1){
-                    if(board[i][j] == 'O'){
+        int n = board.size();
+        int m = board[0].size();
+        vector<vector<int>> vis(n,vector<int>(m,0));
+        queue<pair<int,int>> q;
+        for(int i = 0;i < n ; i++) {
+            for(int j =0 ; j<m ; j++){
+                if(i==0 || i == n-1 || j == 0 || j == m-1) {
+                    if(board[i][j] == 'O') {
                         q.push({i,j});
                         vis[i][j] = 1;
                     }
                 }
             }
         }
-        while(!q.empty()){
+        while(!q.empty()) {
             int r = q.front().first;
             int c = q.front().second;
-            
             q.pop();
-            
-            for(int a=-1;a<=1;a++){
-                for(int b=-1;b<=1;b++){
-                    if(abs(a) == 1 && abs(b) == 1) continue;
-                    
-                    int nr = r + a;
-                    int nc = c + b;
-                    if(nr>=0 && nr<n && nc>=0 &&nc<m 
+
+            for(int i =-1; i<=1 ; i++) {
+                for(int j =-1; j<= 1 ; j++) {
+                    if(abs(i) == abs(j)) continue;
+                    int nr = r+i;
+                    int nc = c+j;
+                    if(nr>=0 && nr < n && nc >= 0 && nc < m 
                     && !vis[nr][nc] && board[nr][nc] == 'O'){
-                        vis[nr][nc] =1;
                         q.push({nr,nc});
+                        vis[nr][nc] = 1;
                     }
                 }
             }
         }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(!vis[i][j] && board[i][j] == 'O') {
+        for(int i = 0; i< n ; i++ ) {
+            for(int j=0; j<m;j++) {
+                if(board[i][j] == 'O' && !vis[i][j]){
                     board[i][j] = 'X';
                 }
             }
         }
-        // return mat;
     }
 };
