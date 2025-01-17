05db9e265d25cf6f0077c3295659d1578a78004d
Kartik shah <123581808+KartikShah369@users.noreply.github.com>
2025-01-18 01:13:30 +0530

    Time: 886 ms (5.06%) | Memory: 31.8 MB (73.11%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -1,39 +1,43 @@
 class Solution {
 public:
-
-    void dfs(int r , int c , vector<vector<int>>& grid , vector<vector<int>>& vis,int n ,int m){
-        vis[r][c] = 1;
-        for(int a =-1;a<=1;a++){
-            for(int b=-1;b<=1;b++){
-                if(abs(a) == 1 && abs(b) == 1) continue;
-                int nr = r +a;
-                int nc = c + b;
-                if(nr>=0 && nr<n && nc>=0 && nc<m &&
-                !vis[nr][nc] && grid[nr][nc] ==1){
-                    dfs(nr,nc,grid,vis,n,m);
-                }
-            }
-        }
-    }
-
     int numEnclaves(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<int> > vis(n,vector<int> (m,0));
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(i==0 || i==n-1 || j==0 || j==m-1){
-                    if(grid[i][j] == 1){
-                       dfs(i,j,grid,vis,n,m);
+        vector<vector<int>> vis(n,vector<int>(m,0));
+        queue<pair<int,int>> q;
+        for(int i= 0; i<n ; i++) {
+            for(int j=0; j < m ; j++) {
+                if(i==0 || j==0 || i == n-1 || j == m-1){
+                    if(grid[i][j] == 1) {
+                        vis[i][j] = 1;
+                        q.push({i,j});
                     }
                 }
             }
         }
-        int cnt=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(!vis[i][j] && grid[i][j] ==1) cnt++;
+        while(!q.empty()){
+            int r = q.front().first;
+            int c = q.front().second;
+            q.pop();
+            for(int i =-1 ;i <= 1; i++){
+                for(int j =-1 ; j <=1; j++){
+                    if(abs(i) == abs(j)) continue;
+                    int nr = r+i;
+                    int nc = c+j;
+                    if(nr>=0 && nc>=0 && nr < n && nc<m && !vis[nr][nc] && grid[nr][nc] == 1){
+                        q.push({nr,nc});
+                        vis[nr][nc] = 1;
+                    }
+                }
             }
+        }
+        int cnt = 0;
+        for(int i = 0; i< n; i++) {
+            for(int j=0; j < m ; j++) {
+                cout<<vis[i][j]<<" ";
+                if(!vis[i][j] && grid[i][j]) cnt++;
+            }
+            cout<<endl;
         }
         return cnt;
     }
