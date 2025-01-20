a01aab61b076b282b60ebee3502f8f0c95337039
Kartik shah <123581808+KartikShah369@users.noreply.github.com>
2025-01-20 14:47:20 +0530

    Time: 219 ms (14.63%) | Memory: 172.2 MB (22.87%) - LeetSync

---
+++ 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
+        int n = mat.size();
+        int m = mat[0].size();
+        map<int , pair<int,int>> mp;
+        for(int i = 0; i < n; i++) {
+            for(int j =0 ;j<m;j++) {
+                mp[mat[i][j]] = {i,j};
+            }
+        }
+        vector<int> r(n,0);
+        vector<int> c(m,0);
+
+        vector<vector<int>> vis(n,vector<int>(m,0));
+        for(int i = 0 ;i < m*n; i++) {
+            int nr = mp[arr[i]].first;
+            int nc = mp[arr[i]].second;
+            r[nr]++;
+            c[nc]++;
+            if(r[nr] == m || c[nc] == n) return i; 
+
+
+
+        }
+        return 0;
+    }
+};
