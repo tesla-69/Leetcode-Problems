2442c6d766652de14c872b55e3e8080406a8abbb
Kartik shah <123581808+KartikShah369@users.noreply.github.com>
2025-01-29 00:35:17 +0530

    Time: 12 ms (47.81%) | Memory: 96.5 MB (63.22%) - LeetSync

--- 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.cpp
+++ 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.cpp
@@ -1,34 +1,46 @@
 class Solution {
-public:
-    int bfs(int i, int j, vector<vector<int>>& grid) {
-        vector<vector<int>> dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        queue<pair<int, int>> q;
-        int n = grid.size(), m = grid[0].size();
-        q.push({i, j});
-        int fish = grid[i][j];
-        grid[i][j] = 0;
-
-        while (!q.empty()) {
-            int x = q.front().first, y = q.front().second;
-            q.pop();
-            for (auto d : dir) {
-                int a = x + d[0], b = y + d[1];
-                if (a >= 0 && a < n && b >= 0 && b < m && grid[a][b]) {
-                    q.push({a, b});
-                    fish += grid[a][b];
-                    grid[a][b] = 0;
+private:
+    void dfs(vector<vector<int>>& grid, vector<vector<int>>& vis, int i,int j, int& curr,
+             int& ans) {
+                 int n = grid.size();
+        int m = grid[0].size();
+        vis[i][j] = 1;
+        curr += grid[i][j];
+        
+        
+        for(int r =-1 ; r<=1;r++) {
+            for(int c = -1;c<=1;c++) {
+                if(abs(r) == abs(c)) continue;
+                int nr = r+i;
+                int nc = c+j;
+                if(nr>=0&&nr<n && nc<m && nc>=0 && !vis[nr][nc] && grid[nr][nc] > 0) {
+                    dfs(grid,vis,nr,nc,curr,ans);
                 }
             }
         }
-        return fish;
+        // curr -= grid[i][j];
+        // vis[i][j] = 0;
     }
 
+public:
     int findMaxFish(vector<vector<int>>& grid) {
-        int n = grid.size(), m = grid[0].size(), mxFish = 0;
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < m; j++)
-                if (grid[i][j])
-                    mxFish = max(mxFish, bfs(i, j, grid));
-        return mxFish;
+        int n = grid.size();
+        int m = grid[0].size();
+        int ans = 0;
+        int cnt = 0;
+        vector<vector<int>> vis(n, vector<int>(m,0));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                // if(grid[i][j] == 0) cnt++;
+                if (!vis[i][j] && grid[i][j] != 0) {
+        int curr = 0;
+                    dfs(grid, vis, i,j, curr, ans);
+                    ans = max(ans,curr);
+
+                }
+            }
+        }
+        // if(cnt == 0) dfs(grid,vis,0,0,curr,ans);
+        return ans;
     }
 };
