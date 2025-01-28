20c25bfe866c0016dac658b96cc3d30f42d77839
Kartik shah <123581808+KartikShah369@users.noreply.github.com>
2025-01-29 00:36:15 +0530

    Time: 4 ms (78.63%) | Memory: 96.5 MB (56.04%) - LeetSync

--- 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.cpp
+++ 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.cpp
@@ -1,20 +1,21 @@
 class Solution {
 private:
-    void dfs(vector<vector<int>>& grid, vector<vector<int>>& vis, int i,int j, int& curr,
-             int& ans) {
-                 int n = grid.size();
+    void dfs(vector<vector<int>>& grid, vector<vector<int>>& vis, int i, int j,
+             int& curr, int& ans) {
+        int n = grid.size();
         int m = grid[0].size();
         vis[i][j] = 1;
         curr += grid[i][j];
-        
-        
-        for(int r =-1 ; r<=1;r++) {
-            for(int c = -1;c<=1;c++) {
-                if(abs(r) == abs(c)) continue;
-                int nr = r+i;
-                int nc = c+j;
-                if(nr>=0&&nr<n && nc<m && nc>=0 && !vis[nr][nc] && grid[nr][nc] > 0) {
-                    dfs(grid,vis,nr,nc,curr,ans);
+
+        for (int r = -1; r <= 1; r++) {
+            for (int c = -1; c <= 1; c++) {
+                if (abs(r) == abs(c))
+                    continue;
+                int nr = r + i;
+                int nc = c + j;
+                if (nr >= 0 && nr < n && nc < m && nc >= 0 && !vis[nr][nc] &&
+                    grid[nr][nc] > 0) {
+                    dfs(grid, vis, nr, nc, curr, ans);
                 }
             }
         }
@@ -28,15 +29,14 @@ public:
         int m = grid[0].size();
         int ans = 0;
         int cnt = 0;
-        vector<vector<int>> vis(n, vector<int>(m,0));
+        vector<vector<int>> vis(n, vector<int>(m, 0));
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 // if(grid[i][j] == 0) cnt++;
                 if (!vis[i][j] && grid[i][j] != 0) {
-        int curr = 0;
-                    dfs(grid, vis, i,j, curr, ans);
-                    ans = max(ans,curr);
-
+                    int curr = 0;
+                    dfs(grid, vis, i, j, curr, ans);
+                    ans = max(ans, curr);
                 }
             }
         }
