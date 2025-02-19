1fa088162cf822d043a6548af92853d5940ccce4
Kartik shah <123581808+KartikShah369@users.noreply.github.com>
2025-02-20 03:50:43 +0530

    Time: 11 ms (15.06%) | Memory: 12 MB (30.15%) - LeetSync

---
+++ 383-ransom-note/ransom-note.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        map<char,int>mp;
+        int n = magazine.length();
+        for(int i=0;i < n;i++){
+            mp[magazine[i]]++;
+        }
+        int m = ransomNote.length();
+        for(int i =0;i<m;i++){
+
+            if(mp[ransomNote[i]] == 0) return false;
+            mp[ransomNote[i]]--;
+        }
+        return true;
+    }
+};
