d9437c27efca68b79b56046a3209ba9a4a439dc5
Kartik shah <123581808+KartikShah369@users.noreply.github.com>
2025-02-22 04:45:34 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (66.11%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -5,22 +5,8 @@ public:
         while(low<=high){
             int mid=(low+high)>>1;
             if(nums[mid]==target)return mid;
-            if(nums[low]<=nums[mid]){
-                if(target>=nums[low] && target<=nums[mid]){
-                    high=mid-1;
-                }
-                else{
-                    low=mid+1;
-                }
-            }
-            else{
-                if(target>=nums[mid] && target<=nums[high]){
-                    low=mid+1;
-                }
-                else{
-                    high=mid-1;
-                }
-            }
+            if(nums[low]<=nums[mid]) (target>=nums[low] && target<=nums[mid]) ? high=mid-1 : low=mid+1;
+            else (target>=nums[mid] && target<=nums[high]) ? low=mid+1 : high=mid-1;
         }
         return -1;
     }
