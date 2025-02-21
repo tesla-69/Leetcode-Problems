4d78f4b5778e9c24577b88ab55b0bfb8badd56ab
Kartik shah <123581808+KartikShah369@users.noreply.github.com>
2025-02-21 06:00:43 +0530

    Time: 3 ms (35.30%) | Memory: 77 MB (72.69%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -11,28 +11,32 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* dummyHead = new ListNode(0);
-        ListNode* tail = dummyHead;
-        int carry = 0;
-
-        while (l1 != nullptr || l2 != nullptr || carry != 0) {
-            int digit1 = (l1 != nullptr) ? l1->val : 0;
-            int digit2 = (l2 != nullptr) ? l2->val : 0;
-
-            int sum = digit1 + digit2 + carry;
-            int digit = sum % 10;
-            carry = sum / 10;
-
-            ListNode* newNode = new ListNode(digit);
-            tail->next = newNode;
-            tail = tail->next;
-
-            l1 = (l1 != nullptr) ? l1->next : nullptr;
-            l2 = (l2 != nullptr) ? l2->next : nullptr;
+        ListNode* head = NULL;
+        ListNode* tail = NULL;
+        int c = 0;
+        while (l1 != NULL || l2 != NULL || c){
+            ListNode* temp = new ListNode();
+            int data = c;
+            if(l1 != NULL){
+                data += l1->val;
+                l1 = l1->next;
+            } 
+            if(l2 != NULL){
+                data += l2->val;
+                l2 = l2->next;
+            } 
+            temp->val = data % 10;
+            c = data / 10;
+            if(head == NULL) {
+                head = temp;
+                tail = temp;
+            }
+            else {
+                tail->next = temp;
+                tail = temp;
+            }
+            
         }
-
-        ListNode* result = dummyHead->next;
-        delete dummyHead;
-        return result;
+        return head;
     }
 };
