a27bb4c00f0b7bf9402460ac31dda09ac20a7a42
Kartik shah <123581808+KartikShah369@users.noreply.github.com>
2025-03-01 23:45:53 +0530

    Time: 1 ms (27.27%) | Memory: 182.2 MB (0.00%) - LeetSync

--- 3785-find-the-number-of-copy-arrays/find-the-number-of-copy-arrays.cpp
+++ 3785-find-the-number-of-copy-arrays/find-the-number-of-copy-arrays.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     int countArrays(vector<int>& original, vector<vector<int>>& bounds) {
-        int cnt = INT_MAX;
         int n = original.size();
         int l = bounds[0][0];
         int h = bounds[0][1];
-        cnt = max(h-l+1,0);
+        int cnt = max(h-l+1,0);
         for(int i = 1; i<n; i++) {
             int diff = original[i] - original[i-1];
             l = max(l + diff , bounds[i][0]);
             h = min(h + diff , bounds[i][1]);
-            cout<<l<< " "<<h<<" "<<endl;
             cnt = min(cnt,h-l+1);
-            cout<<cnt<< " ";
-            cout<<endl;
         }
         cnt = max(cnt,0);
         return cnt;
