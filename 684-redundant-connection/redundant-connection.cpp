321273ca9958e230a69faeffc9b651b00fa8a09d
Kartik shah <123581808+KartikShah369@users.noreply.github.com>
2025-03-26 23:53:04 +0530

    Time: 4 ms (32.80%) | Memory: 13.4 MB (43.94%) - LeetSync

--- 684-redundant-connection/redundant-connection.cpp
+++ 684-redundant-connection/redundant-connection.cpp
@@ -1,40 +1,74 @@
+class disjointSet
+{
+    vector<int> parent, rank, size;
+
+public:
+    disjointSet(int n)
+    {
+        parent.resize(n + 1);
+        rank.resize(n + 1, 0);
+        size.resize(n + 1, 1);
+        for (int i = 0; i <= n; i++)
+            parent[i] = i;
+    }
+    int findUPar(int node)
+    {
+        if (parent[node] == node)
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+    void unionByRank(int u, int v)
+    {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v)
+            return;
+        if (rank[ulp_u] < rank[ulp_v])
+            parent[ulp_u] = ulp_v;
+        else if (rank[ulp_v] < rank[ulp_u])
+            parent[ulp_v] = ulp_u;
+        else
+        {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+    void unionBySize(int u, int v)
+    {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v)
+            return;
+        if (size[ulp_u] < size[ulp_v])
+        {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else
+        {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
 class Solution {
 public:
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-        unordered_map<int, vector<int>> graph;
-
-        auto isConnected = [&](int u, int v) {
-            unordered_set<int> visited;
-            stack<int> stack;
-            stack.push(u);
-
-            while (!stack.empty()) {
-                int node = stack.top();
-                stack.pop();
-
-                if (visited.count(node)) continue;
-                visited.insert(node);
-
-                if (node == v) return true;
-
-                for (int neighbor : graph[node]) {
-                    stack.push(neighbor);
-                }
-            }
-            return false;
-        };
-
-        for (const auto& edge : edges) {
-            int u = edge[0], v = edge[1];
-
-            if (graph.count(u) && graph.count(v) && isConnected(u, v)) {
-                return edge;
-            }
-
-            graph[u].push_back(v);
-            graph[v].push_back(u);
+        vector<int> ans;
+        int n = 0;
+        for(auto it : edges) {
+            n = max({n,it[0],it[1]});
         }
-
-        return {};
+        disjointSet ds(n);
+        for(auto it : edges) {
+            if(ds.findUPar(it[0]) != ds.findUPar(it[1])){
+                ds.unionBySize(it[0], it[1]);
+            }
+            else {
+                ans = {it[0], it[1]};
+            }
+        }
+        return ans;
     }
 };
