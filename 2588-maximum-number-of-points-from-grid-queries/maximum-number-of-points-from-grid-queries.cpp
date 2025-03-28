86d552e2ef67c084cc2078885335437516cfcdfc
Kartik shah <123581808+KartikShah369@users.noreply.github.com>
2025-03-28 23:56:50 +0530

    Time: 179 ms (29.35%) | Memory: 46.6 MB (41.67%) - LeetSync

--- 2588-maximum-number-of-points-from-grid-queries/maximum-number-of-points-from-grid-queries.cpp
+++ 2588-maximum-number-of-points-from-grid-queries/maximum-number-of-points-from-grid-queries.cpp
@@ -1,23 +1,23 @@
 class Solution {
-    void dfs(vector<vector<int>>& grid, int r, int c, int node,
-             vector<vector<int>>& vis, int& val, int n, int m) {
-        vis[r][c] = 1;
-        if (grid[r][c] < node) {
-            val++;
-            for (int i = -1; i <= 1; i++) {
-                for (int j = -1; j <= 1; j++) {
-                    if (abs(i) == 1 && abs(j) == 1)
-                        continue;
-                    int nr = r + i;
-                    int nc = c + j;
-                    if (nr >= 0 && nc >= 0 && nc < m && nr < n &&
-                        !vis[nr][nc]) {
-                        dfs(grid, nr, nc, node, vis, val, n, m);
-                    }
-                }
-            }
-        }
-    }
+    // void dfs(vector<vector<int>>& grid, int r, int c, int node,
+    //          vector<vector<int>>& vis, int& val, int n, int m) {
+    //     vis[r][c] = 1;
+    //     if (grid[r][c] < node) {
+    //         val++;
+    //         for (int i = -1; i <= 1; i++) {
+    //             for (int j = -1; j <= 1; j++) {
+    //                 if (abs(i) == 1 && abs(j) == 1)
+    //                     continue;
+    //                 int nr = r + i;
+    //                 int nc = c + j;
+    //                 if (nr >= 0 && nc >= 0 && nc < m && nr < n &&
+    //                     !vis[nr][nc]) {
+    //                     dfs(grid, nr, nc, node, vis, val, n, m);
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
 
 public:
     vector<int> maxPoints(vector<vector<int>>& grid, vector<int>& queries) {
@@ -39,7 +39,6 @@ public:
         vector<int> ans(qs);
         for (int i = 0; i < qs; i++) {
             int cnt = 0;
-            cout<<"q size : "<<pq.size()<<endl;
             while (!pq.empty()) {
                 auto it = pq.top();
                 int val = it.first;
@@ -49,7 +48,7 @@ public:
                     pq.pop();
                     continue;
                 }
-                cout<<"r = "<<r<<" c = "<<c<<endl;
+                // cout<<"r = "<<r<<" c = "<<c<<endl;
                 if (val < arr[i].first) {
                     pq.pop();
                     cnt++;
@@ -67,16 +66,16 @@ public:
                     }
                 }
                 else{
-                    cout<<"break"<<endl;
+                    // cout<<"break"<<endl;
                     break;
                 } 
             }
             if(i == 0) ans[arr[i].second] = cnt;
             else{
-                cout<<"ans "<<ans[arr[i-1].second] << "cnt: " <<cnt<<endl;
+                // cout<<"ans "<<ans[arr[i-1].second] << "cnt: " <<cnt<<endl;
                 ans[arr[i].second] = ans[arr[i-1].second] + cnt;
             }
-            cout<<endl;
+            // cout<<endl;
         }
 
         return ans;
