39937eef6f369fe7d854d17e3eee997c958152f0
Kartik shah <123581808+KartikShah369@users.noreply.github.com>
2025-04-05 05:00:54 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (17.38%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,16 +1,18 @@
 class Solution {
+    int noOfWays(int n, vector<int>&arr) {
+        if(n == 1) return arr[n] = 1;
+        if(arr[n] != -1) return arr[n]; 
+        int l = noOfWays(n-1, arr);
+        int r = noOfWays(n-2, arr);
+        return arr[n] = l+r;
+    }
 public:
     int climbStairs(int n) {
-        vector<int> arr(n + 1, -1); // Ensure the array size is exactly needed
-        
-        return helper(n, arr);
-    }
-
-private:
-    int helper(int n, vector<int>& arr) {
-        if (n == 0 || n == 1) return 1;
-        if (arr[n] != -1) return arr[n];
-
-        return arr[n] = helper(n - 1, arr) + helper(n - 2, arr);
+        vector<int> dp(n+1, 0);
+        dp[0] = 1, dp[1] = 1;
+        for(int i = 2; i <= n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 };
