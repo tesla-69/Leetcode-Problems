e1fa6c467105e8138b96d7851c4c09b03e01b519
Kartik shah <123581808+tesla-69@users.noreply.github.com>
2025-04-06 17:50:59 +0530

    Time: 12 ms (45.23%) | Memory: 12.6 MB (47.06%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.cpp
+++ 368-largest-divisible-subset/largest-divisible-subset.cpp
@@ -1,37 +1,22 @@
 class Solution {
 public:
-
-void solve(vector<int>& nums, int n, vector<int>&temp) {
-	vector<int> dp(n, 1), hash(n);
-	int maxi = 1;
-	int lastIndex = 0;
-   sort(nums.begin(), nums.end());
-	for (int i = 0; i < n; i++) {
-		hash[i] = i;
-		for (int prev = 0; prev < i; prev ++) {
-			if (nums[i] % nums[prev] == 0 &&  1 +  dp[prev] > dp[i]) {
-				dp[i] = 1 +  dp[prev];
-				hash[i] = prev;
-			}
-		}
-		if (dp[i] > maxi) {
-			maxi = dp[i];
-			lastIndex = i;
-		}
-	}
-
-	temp.push_back(nums[lastIndex]);
-	while (hash[lastIndex] != lastIndex) {
-		lastIndex = hash[lastIndex];
-		temp.push_back(nums[lastIndex]);
-	}
-
-}
-
     vector<int> largestDivisibleSubset(vector<int>& nums) {
-        int n = nums.size();
-        vector<int>temp;
-        solve(nums, n, temp);
-        return temp;
+        sort(nums.begin(), nums.end());
+        vector<int> dp(nums.size(), 1), prev(nums.size(), -1);
+        int maxi = 0;
+        for (int i = 1; i < nums.size(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] % nums[j] == 0 && dp[i] < dp[j] + 1) {
+                    dp[i] = dp[j] + 1;
+                    prev[i] = j;
+                }
+            }
+            if (dp[i] > dp[maxi]) maxi = i;
+        }
+        vector<int> res;
+        for (int i = maxi; i >= 0; i = prev[i]) {
+            res.push_back(nums[i]);
+        }
+        return res;
     }
 };
