35f4708c7608bbc5c22a3ba9782b28c627b1c9a3
Kartik shah <123581808+tesla-69@users.noreply.github.com>
2025-05-05 05:59:52 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (86.33%) - LeetSync

---
+++ 806-domino-and-tromino-tiling/domino-and-tromino-tiling.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+	const int MOD = 1e9+7;
+    int numTilings(int n) {
+        if(n <= 2) return n;
+		// filled_prev = dp[i-1][0], filled_prev2 = dp[i-2][0]...same for gap_prev & gap_prev2
+        int filled_prev = 2, gap_prev = 2, filled_prev2 = 1, gap_prev2 = 1;
+        for(int i = 3; i <= n; i++) {
+            int filled = (filled_prev + filled_prev2 + 2l*gap_prev2) % MOD;
+            int gap = (filled_prev + gap_prev) % MOD;
+            
+            filled_prev2 = filled_prev, filled_prev = filled;
+            gap_prev2 = gap_prev, gap_prev = gap;
+        }
+        return filled_prev;
+    }
+};
