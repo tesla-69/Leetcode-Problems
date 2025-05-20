a4872395e9fc1983d41135c33c9d0211bfda41c5
Kartik shah <123581808+tesla-69@users.noreply.github.com>
2025-05-21 02:19:46 +0530

    Time: 153 ms (10.67%) | Memory: 325.3 MB (29.56%) - LeetSync

--- 3639-zero-array-transformation-i/zero-array-transformation-i.cpp
+++ 3639-zero-array-transformation-i/zero-array-transformation-i.cpp
@@ -6,19 +6,13 @@ public:
         for(auto it: queries) {
             int i = it[0];
             int j = it[1];
-            // cout<<i<<" "<<j<<endl;
             diffArr[i] += 1;
             if(j != n-1) diffArr[j+1] -= 1;
         }
         for(int i = 1; i < n; i++) {
             diffArr[i] += diffArr[i-1];
-        }
-        // for(int i = 0; i < n; i++){
-        //     cout<<diffArr[i]<<" ";
-        // }
-        for(int i = 0; i < n; i++) {
             if(nums[i] > diffArr[i]) return false;
         }
-        return true;
+        return nums[0] <= diffArr[0];
     }
 };
