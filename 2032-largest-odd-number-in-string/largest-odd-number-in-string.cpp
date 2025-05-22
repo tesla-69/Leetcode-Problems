cfee1771d622fabd2d2d79895bbbe7aa7d0f0935
Kartik shah <123581808+tesla-69@users.noreply.github.com>
2025-05-22 21:00:22 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (81.73%) - LeetSync

---
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string largestOddNumber(string num) {
+        int n = num.size();
+        for(int i = n-1; i>=0; i--) {
+            int a = num[i] - '0';
+            if(a & 1) return num.substr(0, i+1);
+        }
+        return "";
+    }
+};
