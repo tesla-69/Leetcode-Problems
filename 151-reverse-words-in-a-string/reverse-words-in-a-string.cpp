552eaa4ea1df5993778f10d39b42f0041e3cc862
Kartik shah <123581808+tesla-69@users.noreply.github.com>
2025-05-22 19:12:59 +0530

    Time: 1 ms (53.29%) | Memory: 10.6 MB (49.14%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -5,27 +5,25 @@ public:
         int n = s.size();
         int i = 0;
         while(i < n) {
-            cout<<i<<" ";
+            // cout<<i<<" ";
             while(i < n && s[i] == ' ') i++;
-            cout<<i<<" ";
+            // cout<<i<<" ";
             string temp;
             while( i < n && s[i] != ' ') {
                 temp += s[i];
                 i++;
             }
-            cout<<temp<<endl;
-            if(temp != "")
-            st.push(temp);
+            // cout<<temp<<endl;
+            if(temp != "") st.push(temp);
         }
         string ans;
         while(!st.empty()) {
-            cout<<st.top()<<endl;
+            // cout<<st.top()<<endl;
             ans += st.top();
             st.pop();
             ans += ' ';
         }
         ans.pop_back();
         return ans;
-        
     }
 };
