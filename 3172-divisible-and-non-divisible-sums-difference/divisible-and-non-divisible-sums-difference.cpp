46f283b15feac83036f951105bf93b505eb1b94c
Kartik shah <123581808+tesla-69@users.noreply.github.com>
2025-05-27 19:55:47 +0530

    Time: 2 ms (7.21%) | Memory: 8.5 MB (4.56%) - LeetSync

--- 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.cpp
+++ 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.cpp
@@ -4,9 +4,7 @@ public:
         int num1 = 0, num2 = 0;
         for(int i = 1; i <= n; i++) {
             if(i % m != 0) num1 += i;
-        }
-        for(int i = 1; i <= n; i++) {
-            if(i % m == 0) num2 += i;
+            else num2 += i;
         }
         return num1 - num2;
     }
