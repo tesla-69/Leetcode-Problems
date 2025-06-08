8cf17525f3310865d5ca609f14f19a22df19d5c0
Kartik shah <123581808+tesla-69@users.noreply.github.com>
2025-06-08 11:51:41 +0530

    Time: 147 ms (35.98%) | Memory: 17.4 MB (61.33%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
-        int totalSum = 0;
-        for (int num : nums) totalSum += num;
+        int sum = 0;
+        int n = nums.size();
+        for(int i = 0 ; i < n; i++) sum += nums[i];
+        if(sum & 1) return false;
+        sum /= 2;
+        vector<vector<bool>> dp(n, vector<bool>(sum+1, false));
 
-        if (totalSum % 2 != 0) return false;
+        for(int i = 0 ; i < n; i++) dp[i][0] = true;
+        if(nums[0] <= sum) dp[0][nums[0]] = true;
 
-        int targetSum = totalSum / 2;
-        vector<bool> dp(targetSum + 1, false);
-        dp[0] = true;
-        for (int num : nums) {
-            for (int currSum = targetSum; currSum >= num; --currSum) {
-                dp[currSum] = dp[currSum] || dp[currSum - num];
-                if (dp[targetSum]) return true;
+        for(int i = 1; i < n; i++) {
+            for(int j = 1; j <= sum; j++) {
+                bool nt = dp[i-1][j];
+                bool t = false;
+                if(nums[i] <= j) t = dp[i-1][j - nums[i]];
+                dp[i][j] = nt || t;
             }
         }
-        return dp[targetSum];
+        return dp[n-1][sum];
     }
 };
