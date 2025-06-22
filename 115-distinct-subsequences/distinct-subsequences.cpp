47c8b420555877f43eef870ea63a633dc9bb32e8
Kartik shah <123581808+tesla-69@users.noreply.github.com>
2025-06-23 01:40:27 +0530

    Time: 33 ms (40.97%) | Memory: 44.2 MB (39.09%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,23 +1,37 @@
 class Solution {
 public:
+    int solve(int i, int j, string& s, string& t, vector<vector<int>>& dp) {
+        if (j == 0)
+            return 1;
+        if (i == 0)
+            return 0;
 
-    int solve(int i, int j, string &s, string &t, vector<vector<int>>& dp) {
-        if(j == 0) return 1;
-        if(i == 0) return 0;
+        if (dp[i][j] != -1)
+            return dp[i][j];
 
-        if(dp[i][j] != -1) return dp[i][j];
-
-        if(s[i-1] == t[j-1]) {
-            return dp[i][j] = solve(i-1, j-1, s, t, dp) + solve(i-1, j, s, t, dp);
-        }
-        else return dp[i][j] = solve(i-1, j, s, t, dp);
+        if (s[i - 1] == t[j - 1]) {
+            return dp[i][j] = solve(i - 1, j - 1, s, t, dp) +
+                              solve(i - 1, j, s, t, dp);
+        } else
+            return dp[i][j] = solve(i - 1, j, s, t, dp);
     }
 
     int numDistinct(string s, string t) {
         int n = s.size();
         int m = t.size();
-        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
-        return solve(n, m, s, t, dp);
+        vector<vector<long long>> dp(n + 1, vector<long long>(m + 1, 0));
+        // return solve(n, m, s, t, dp);
+        for (int i = 0; i <= n; i++)
+            dp[i][0] = 1;
 
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s[i - 1] == t[j - 1]) {
+                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j])%INT_MAX;
+                } else
+                    dp[i][j] = dp[i - 1][j];
+            }
+        }
+        return dp[n][m];
     }
 };
